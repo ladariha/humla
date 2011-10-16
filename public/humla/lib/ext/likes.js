ae2e52009678922fb43a93fd7e2c0d2d3e1f7a8e
Petr Mikota <bubersson@gmail.com>
2011-10-16 22:06:20 +0200

    Added comments css

---
+++ public/humla/lib/ext/likes.js
@@ -0,0 +1,16 @@
+/**
+ * Likes Extension
+ * ~~~~~~~~~~~~~~~
+ * adds like / unlike button under each slide
+ *
+ */
+
+var ex_likes = {    
+    
+    processSlide : function(slide) {
+        
+        
+        
+    }
+    
+}
