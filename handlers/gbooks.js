7e0090c982cd0f6f92a7fe281aeda12acdc082cc
ladariha <riha.vladimir@gmail.com>
2011-12-23 21:18:24 +0100

    extension for Google Books (so far 2 templates, next are comming), wiki with code samples and usages will be updated in a few days

--- handlers/gbooks.js
+++ handlers/gbooks.js
@@ -1,10 +1,9 @@
 var https = require('https');
 
-
-app.get('/api/gbooks/:id', function(req, res){
+app.get('/api/gbooks/:id/:mode', function(req, res){
     
     var id = req.params.id;
-    console.log(id);
+    var mode = req.params.mode;
     var options = {
         host: 'www.googleapis.com',
         port: 443,
@@ -14,18 +13,13 @@ app.get('/api/gbooks/:id', function(req, res){
     var content = '';
     var request = https.request(options, function(res2) {
         res2.setEncoding('utf8'); 
-        statusCode = res2.statusCode;
         res2.on('data', function (chunk) {
             content += chunk;
         });
 
         res2.on('end', function () {
             if(res2.statusCode === 200){
-                res.writeHead(200, {
-                    'Content-Type': 'application/json'
-                });
-                res.write(content);
-                res.end();  
+                generateGbooksOutput(mode, content, req, res);
             }else{
                 res2.writeHead(res2.statusCode, {
                     'Content-Type': 'text/plain'
@@ -46,3 +40,113 @@ app.get('/api/gbooks/:id', function(req, res){
     });
     
 });
+
+
+function generateGbooksOutput(mode, content, req, res){
+    
+    switch(mode){
+        case "0":
+            generateGbooksSimpleList(content, req, res);
+            break;
+        case "1":
+            generateGbooksBigThumbnal(content, req, res);
+            break;
+        default:
+            res.writeHead(400, {
+                'Content-Type': 'text/plain'
+            });
+            res.write("Not supported mode "+mode);
+            res.end();    
+    }
+}
+
+function generateGbooksSimpleList(data, req, res){
+    // better ugly variable than IO operation... 
+    var content = "<ul class=\"gbooksList\"><li title=\"Google Books Link\" data-gbooks-isbn=\"##ISBN\" data-gbooks-id=\"##ID\" data-gbooks-category=\"##CATEGORY\"><a href=\"##INFOLINK\"> ##AUTHOR - <span class=\"gbooksTitle\">##TITLE</span>. [##DATE]    </a></li></ul>";
+    
+ 
+    data = eval('(' + data + ')');
+    if(data.volumeInfo.industryIdentifiers[1] !== undefined){
+        content = content.replace("##ISBN", data.volumeInfo.industryIdentifiers[1].identifier);
+    }else{
+        if(content.volumeInfo.industryIdentifiers[0]!== undefined){
+            content = content.replace("##ISBN", data.volumeInfo.industryIdentifiers[0].identifier);    
+        }else{
+            content = content.replace("##ISBN", "-1");    
+        }
+    }
+    content = content.replace("##ID", data.id);
+    content = content.replace("##INFOLINK", data.volumeInfo.infoLink);
+    content = content.replace("##TITLE", data.volumeInfo.title);
+    content = content.replace("##DATE", data.volumeInfo.publishedDate);
+           
+    var temp="";
+    for (var i = 0; i < data.volumeInfo.categories.length; i++)
+        temp +=data.volumeInfo.categories[i]+",";
+           
+    temp = temp.substr(0, temp.length-1);
+    content = content.replace("##CATEGORY", temp);
+
+    temp="";
+    for (var i = 0; i < data.volumeInfo.authors.length; i++)
+        temp +=data.volumeInfo.authors[i]+",";
+           
+    temp = temp.substr(0, temp.length-1);
+    content = content.replace("##AUTHOR", temp);
+            
+    res.writeHead(200, {
+        'Content-Type': 'text/plain'
+    }); 
+    res.write(content);
+    res.end();  
+}
+
+
+function generateGbooksBigThumbnal(data, req, res){
+    // better ugly variable than IO operation... 
+//    var content = "<h4><a href=\"##INFOLINK\">##TITLE</a></h4><div class=\"gbooksColumns2\"><div class=\"gbooksImgLeft\"><img class=\"gbooksImgLarge\" src=\"##IMG\"/></div><div class=\"gbooksRight\" data-gbooks-isbn=\"##ISBN\" data-gbooks-id=\"##ID\" data-gbooks-category=\"##CATEGORY\"><ul class=\"gbooksListSecret\"><li>##AUTHOR </li><li> ##PUBLISHER, ##DATE</li><br/><li class=\"gbooksDesc\">##DESC</li></ul></div></div>";
+    var content = "<div class=\"gbooksBig\"><span><a href=\"##INFOLINK\">##TITLE</a></span><div class=\"gbooksColumns2\"><table><tbody><tr><td><img class=\"gbooksImgLarge\" src=\"##IMG\"/></td><td><div class=\"gbooksRight\" data-gbooks-isbn=\"##ISBN\" data-gbooks-id=\"##ID\" data-gbooks-category=\"##CATEGORY\"><ul class=\"gbooksListSecret\"><li>##AUTHOR </li><li> ##PUBLISHER, ##DATE</li><br/><li class=\"gbooksDesc\">##DESC</li></ul></div></td></tr></tbody></table></div></div>";
+ 
+    data = eval('(' + data + ')');
+    if(data.volumeInfo.industryIdentifiers[1] !== undefined){
+        content = content.replace("##ISBN", data.volumeInfo.industryIdentifiers[1].identifier);
+    }else{
+        if(content.volumeInfo.industryIdentifiers[0]!== undefined){
+            content = content.replace("##ISBN", data.volumeInfo.industryIdentifiers[0].identifier);    
+        }else{
+            content = content.replace("##ISBN", "-1");    
+        }
+    }
+    content = content.replace("##ID", data.id);
+    content = content.replace("##INFOLINK", data.volumeInfo.infoLink);
+    content = content.replace("##TITLE", data.volumeInfo.title);
+    content = content.replace("##IMG", data.volumeInfo.imageLinks.thumbnail);
+    content = content.replace("##PUBLISHER", data.volumeInfo.publisher);
+    if(data.volumeInfo.description.length > 200){
+        content = content.replace("##DESC", data.volumeInfo.description.substring(0,200)+"...");    
+    }else{
+        content = content.replace("##DESC", data.volumeInfo.description);    
+    }
+    
+    content = content.replace("##DATE", data.volumeInfo.publishedDate);
+           
+    var temp="";
+    for (var i = 0; i < data.volumeInfo.categories.length; i++)
+        temp +=data.volumeInfo.categories[i]+",";
+           
+    temp = temp.substr(0, temp.length-1);
+    content = content.replace("##CATEGORY", temp);
+
+    temp="";
+    for (var i = 0; i < data.volumeInfo.authors.length; i++)
+        temp +=data.volumeInfo.authors[i]+",";
+           
+    temp = temp.substr(0, temp.length-1);
+    content = content.replace("##AUTHOR", temp);
+            
+    res.writeHead(200, {
+        'Content-Type': 'text/plain'
+    }); 
+    res.write(content);
+    res.end();  
+}
--- public/humla/lib/ext/gbooks.js
+++ public/humla/lib/ext/gbooks.js
@@ -1,29 +1,35 @@
 var ex_gbooks = {
     
-    enterSlide : function(slide) {
-
-        var processBooks = function(div) {
+    processSlide : function(slide) {
+        var ext = this;
+        var processBooks = function(div, i) {
+            slide.async_cntr[ext.config.id]++;
+            //slide.element.innerHTML = slide.element.innerHTML+'Loading book...';
+            div.innerHTML = 'Loading book...';
             var xhr = new XMLHttpRequest();
-            xhr.open("GET",  "/api/gbooks/"+div.id, true);
-            xhr.onreadystatechange = function(){
-                if (xhr.readyState==4) {
-                    if(xhr.status==200){
-                        var object = eval('(' + xhr.responseText + ')');
-                        console.log(object);
-                    }else{
-                        console.log(xhr.statusText);
-                    }  
-                }
-            }
+            xhr.open("GET",  "/api/gbooks/"+div.id+"/"+div.getAttribute("data-gbooks-mode"), true);
+            xhr.onreadystatechange = generateOutput(xhr, i);
             xhr.send(null);  
             
+            function generateOutput(xhr,i) { 
+                // not pretty, but simple div.innerHTML doesn't work (what user sees remains untouched but source code is changed :/ )
+                return function() {
+                    if (xhr.readyState==4) {     
+                        var divs = slide.element.getElementsByClassName('h-gbooks');
+                        for (var j = 0; j < divs.length; j++){
+                            if(i===j)
+                                divs[j].innerHTML =  xhr.responseText;
+                        }
+                        slide.async_cntr[ext.config.id]--;  
+                    }
+                };
+            }
         };
-
+        
         slide.async_cntr[this.config.id] = 0;
-        divs = slide.element.getElementsByClassName('h-gbooks');
-        console.log("velikost "+divs.length);
+        var divs = slide.element.getElementsByClassName('h-gbooks');
         for (var i = 0; i < divs.length; i++)
-            processBooks(divs[i]);    
+            processBooks(divs[i], i);    
     
     }
 };
