f5852a4eab332b1b81d732c77bee6265bdbb297b
Petr Mikota <bubersson@gmail.com>
2012-03-24 11:48:46 +0100

    Yea, I've forgot to add files from previous commit.

---
+++ humla-server.js
@@ -0,0 +1,68 @@
+/** 
+ * Humla Server
+ * ~~~~~~~~~~~
+ * usage:
+ * var humla = require("humla-server");
+ * humla.run(config);
+ * 
+ * TODO: spojit s index.js ????
+ */
+
+var fs   = require('fs');
+var path = require('path');
+var server = require("./server");
+
+
+//load JSON config file
+//var config=loadConfig('./server-config.json') || {};
+
+
+
+exports.use = function(config) {
+    //initialize server environment
+    server.init(config);
+
+    //server.run(handlers, PORT, WEBROOT);
+    //server.run();
+    
+    
+}
+
+
+
+
+
+
+//Example usage:
+// $ node humla-server.js
+
+if(!module.parent) {    
+    var humla = this; // in another module it would be var humla = require("humla-server");
+    
+    humla.use({   
+        "server":{
+            "runserver":true,
+            "usedb":true,
+            "webroot":"public",
+            "domain":"127.0.0.1",
+            "port" :1338,        
+            "db_uri": "mongodb://localhost/humla",
+            "handlers_relative_path":"./handlers/",
+            "models_relative_path":"./models/"
+        },
+        "plugins": [
+            {"id":null,"enable":true,"src":"./server_ext/slideindex/slideindex_ext.js"},
+            {"id":null,"enable":true,"src":"./server_ext/atom/atom_module_ext.js"},
+            {"id":null,"enable":true,"src":"./server_ext/editor/editor_ext.js"},
+            {"id":null,"enable":true,"src":"./server_ext/facet/facetparser_ext.js"},
+            {"id":null,"enable":true,"src":"./server_ext/facet/facetengine_ext.js"},
+            {"id":null,"enable":true,"src":"./server_ext/microdata/microdataparser_ext.js"},
+            {"id":null,"enable":true,"src":"./server_ext/maintenance/maintenance_lecture_ext.js"},
+            {"id":null,"enable":true,"src":"./server_ext/gbooks/gbooks_ext.js"}
+        ],  
+        "//":"Tady by to asi chtelo prihodit i jednotlive handlery a modely a u kazdeho zase 'enabled'",
+        "handlers":[],
+        "models":[]
+    });
+    
+}
