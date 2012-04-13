31145f1d7abdf52a95615aa341c67b55596d8af8
ladariha <riha.vladimir@gmail.com>
2012-04-13 22:58:42 +0200

    ubuntu installer actually works and it is awesome :) ; added search page link to top menu in frontend

--- ubuntu_debian_installer.sh
+++ ubuntu_debian_installer.sh
@@ -1,19 +1,80 @@
 #!/bin/sh
+echo " _  _ _  _ _  _ _    ____    "
+echo " |__| |  | |\/| |    |__|    "
+echo " |  | |__| |  | |___ |  |    "
+echo "==========================   "
+echo "Humla is an open source presentation & lecture hosting environment based on node.js server. As a presentation frontend, or as we call it a 'client', it uses beautiful HTML5 slides inspired by HTML5Rocks, but with many extra features (Comments, Likes, Tests, Faceted navigation etc.) "
+echo " "
+echo "Authors: Tomas Vitvar, Petr Mikota, Vladimir Riha, Vojtech Smrcek"
+echo "Humla Installer by Vladimir Riha"
+echo "Check newest stuff on our website https://github.com/bubersson/humla"
+echo " "
+echo "This installer will install following software (if not already present):"
+echo "  - curl, nodejs, npm, npm modules, download MongoDB"
+echo "Do you want to install Humla and all dependencies? (y/n):"
+
+read ctd
+
+ if [ "$ctd" = "y" ]; then
+    	echo "Starting installation..."
+    else
+    	echo "Installation canceled..."
+    	exit 1
+    fi
+
+ if [ "$(id -u)" != "0" ]; then
+    echo "$0 must be run as root..."
+    exit 1
+  fi
+
 cd ~
 if [ "${#1}" -gt 0 ]; then
 if [ -d "$1" ]; then
     echo "Directory already exist..."
-    exit;
+    exit 1
 fi
+
+if command -v g++ >/dev/null 2>&1; then
+	echo "g++ installed..."
+else
+	echo "g++ is not installed. Some npm modules require it. Do you want to install it? (y/n)"
+	read gres
+	 if [ "$gres" = "y" ]; then
+    	echo "Installing g++..."
+    	sudo apt-get install g++
+    	echo "... g++ installed"
+    else
+    	echo "cancelling installation. Please install g++ first..."
+    	exit 1
+    fi
+ 	
+fi
+
+
+if command -v curl >/dev/null 2>&1; then
+    echo "cURL already installed, skipping cURL installation..."
+else
+    echo "cURL is missing, do you want to install it? (y/n):"
+    read res
+    if [ "$res" = "y" ]; then
+    	echo "Installing cURL..."
+    	sudo apt-get install curl
+    	echo "... cURL installed"
+    else
+    	echo "Installation canceled..."
+    	exit 1
+    fi
+fi
+
 mkdir $1 && cd ./$1
 
 # GETTING HUMLA FROM GITHUB
-
-	if command -v git >/dev/null 2>&1; then
+echo "Retrieving Humla..."
+if command -v git >/dev/null 2>&1; then
 	git clone https://github.com/bubersson/humla.git
 else
-	echo >&2 "No git found, trying to use wget to download archive instead..."
-	curl https://github.com/bubersson/humla/tarball/master > master.tar.gz
+	echo >&2 "No git found, trying to use cURL to download archive instead..."
+	curl -L https://github.com/bubersson/humla/tarball/master > master.tar.gz
 	archive=master.tar.gz
     mkdir ${archive%.tar*} 
     tar --extract --file=${archive} --strip-components=1 --directory=${archive%.tar*}
@@ -21,7 +82,7 @@ else
 	rm master.tar.gz
 fi
 
-echo "\n Humla retrieved from github..."
+echo "\nHumla retrieved from github..."
 
 # NODE INSTALLATION
 if command -v node >/dev/null 2>&1; then
@@ -38,7 +99,7 @@ if command -v node >/dev/null 2>&1; then
 		sudo apt-get install nodejs
 		#echo "Installing nodejs-dev"
 		#sudo apt-get install nodejs-dev
-		echo "\n Node.JS installed..."
+		echo "\nNode.JS installed..."
 	fi
 
 # NPM INSTALLATION
@@ -46,6 +107,7 @@ if command -v npm >/dev/null 2>&1; then
 	# NPM EXISTS
 	echo "npm command found, skipping npm installation. Make sure following modules are installed: express, mongoose, jsdom, cron, passport, passport-local and passport-google..."
 	else
+		echo "Installing npm..."
 		curl http://npmjs.org/install.sh | sudo sh
 		npm install express
 		npm install mongoose
@@ -54,15 +116,15 @@ if command -v npm >/dev/null 2>&1; then
 		npm install passport
 		npm install passport-local
 		npm install passport-google
-		echo "\n NPM installed..."
+		echo "\nNPM installed..."
 	fi
 
 # MONGO DB
     echo "Downloading MongoDB..."
-    curl http://downloads.mongodb.org/linux/mongodb-linux-i686-1.6.4.tgz > mongo.tgz
+    curl -L http://downloads.mongodb.org/linux/mongodb-linux-i686-1.6.4.tgz > mongo.tgz
 	archive=mongo.tgz
-    mkdir ${archive%.tar*} 
-    tar --extract --file=${archive} --strip-components=1 --directory=${archive%.tar*}
+    mkdir ${archive%.tgz*} 
+    tar --extract --file=${archive} --strip-components=1 --directory=${archive%.tgz*}
 	rm mongo.tgz
 	
 	## data directory
@@ -71,9 +133,14 @@ if command -v npm >/dev/null 2>&1; then
 
 	echo "MongoDB ready, to start it run './mongo/bin/mongod --dbpath ~/$1/humladb --oplogSize 200 --smallfiles'"
 
-echo "Installation complete, start database and then go to ~/$1 and run 'node index.js'"
+USERNAME=`logname`
+GROUP=`id -g -n $USERNAME`
+
+sudo chown $USERNAME:$GROUP ~/$1 ~/$1/humla ~/$1/mongo
+
+echo "\n\nInstallation complete, start database and then go to ~/$1 and run 'node index.js'"
 
 else
-	echo "\n Specify target directory!"
-	exit;
+	echo "\nSpecify target directory!"
+	exit 1
 fi
